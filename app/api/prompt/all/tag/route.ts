62c6dce84b55a9e4378787e683405d704e7cc119
Eduarda Almeida <eduardapalm27@gmail.com>
2024-06-21 03:29:23 -0300

    feat: Add API route for fetching prompts by tag
    This commit adds a new API route for fetching prompts by tag. The `GET` function in the `route.ts` file handles the request and connects to the database using the `connectToDB` function. It retrieves all prompts with the specified tag and populates the `createdBy` field to include the user information. This feature enhances the functionality of the application by allowing users to retrieve prompts based on a specific tag.

---
+++ app/api/prompt/all/tag/route.ts
@@ -0,0 +1,31 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+export const GET = async (req: Request) => {
+  const url = new URL(req.url);
+  const tag = url.searchParams.get("tag");
+
+  if (!tag) {
+    return new Response(JSON.stringify({ error: "Tag is required" }), {
+      status: 400,
+    });
+  }
+
+  try {
+    await connectToDB();
+
+    const prompts = await Prompt.find({
+      tag: tag,
+    }).populate("createdBy");
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({
+        error: `Failed to Fetch Prompts by Tag. Server Error: ${error}`,
+      }),
+      { status: 500 }
+    );
+  }
+};
