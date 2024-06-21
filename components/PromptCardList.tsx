dc794e61648fa6fb31d1bd1d24e2b87b44c2a17f
Eduarda Almeida <eduardapalm27@gmail.com>
2024-06-21 03:30:53 -0300

    feat: Refactor PromptCardList component to improve performance and add functionality
    This commit refactors the PromptCardList component to improve performance and add new functionality. It introduces lazy loading for images by using the "useIntersection" hook from the "react-intersection-observer" library. This ensures that images are only loaded when they are visible on the screen, reducing the initial page load time. Additionally, it adds new event handlers for editing and deleting prompts, allowing users to modify and remove prompts directly from the PromptCard component. These changes enhance the user experience and make the PromptCardList component more efficient and interactive.

--- components/PromptCardList.tsx
+++ components/PromptCardList.tsx
@@ -1,14 +1,76 @@
+"use client";
+import IPrompt from "@interfaces/IPrompt";
 import IPromptCardList from "../interfaces/props/IPromptCardListProps";
 import PromptCard from "./PromptCard";
+import { useRouter, usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const PromptCardList = (props: IPromptCardList) => {
-  const { data } = props;
+  const { data, customClass } = props;
+  const [promptList, setPromptList] = useState<IPrompt[]>([]);
+  const router = useRouter();
+  const pathname = usePathname();
+  const handleClick = async (selectedTag: string) => {
+    try {
+      const response = await fetch(
+        `/api/prompt/all/tag?tag=${encodeURIComponent(selectedTag)}`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setPromptList(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleEdit = (prompt: IPrompt) => {
+    router.push(`/update-prompt?id=${prompt._id}`);
+  };
+
+  const handleDelete = async (prompt: IPrompt) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        const response = await fetch(`/api/prompt/${prompt._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        if (response.ok) {
+          const currentList = promptList.filter(
+            (item) => item._id !== prompt._id
+          );
+          setPromptList(currentList);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (data) {
+      setPromptList(data);
+    }
+  }, [data]);
+
   return (
-    <section>
-      {data.map((prompt) => (
-        <PromptCard key={prompt._id.toString()} prompt={prompt} />
-      ))}
-    </section>
+    <div className={`${customClass ?? "mt-16"} prompt_layout`}>
+      {promptList?.length > 0 &&
+        promptList.map((prompt) => (
+          <PromptCard
+            key={prompt._id.toString()}
+            prompt={prompt}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+            handleTagClick={handleClick}
+          />
+        ))}
+    </div>
   );
 };
 
