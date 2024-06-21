1c73c8f16222105a92cbea1875b336b51d9ebb4e
Eduarda Almeida <eduardapalm27@gmail.com>
2024-06-21 03:30:30 -0300

    feat: Update Profile component to include name, description, and prompt data
    The code changes in this commit update the Profile component to include the name, description, and prompt data. It imports the IProfileProps interface and receives the props as an argument. The component now renders the name and description in the appropriate HTML elements and passes the prompt data to the PromptCardList component for rendering. This enhancement improves the functionality of the Profile component by displaying the necessary information and providing a visual representation of the prompt data.

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,5 +1,18 @@
-const Profile = () => {
-  return <div>Profile</div>;
+import IProfileProps from "@interfaces/props/IProfileProps";
+import PromptCardList from "./PromptCardList";
+
+const Profile = (props: IProfileProps) => {
+  const { name, description, data } = props;
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="desc text-left">{description}</p>
+
+      <PromptCardList data={data} customClass="mt-10" />
+    </section>
+  );
 };
 
 export default Profile;
